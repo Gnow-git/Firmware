3f5920d5847550f80e15e6fb5ba49683399d1917
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2022-12-09 18:28:14 +0900

    더하기 빼기 구현

--- TermProject.ino
+++ TermProject.ino
@@ -3,7 +3,10 @@
 #include "string.h"
 unsigned char dta[20];
 String data;
-int case_num;
+int case_num, num, init_num = 1;
+int count = 0;
+//unsigned char number[20];
+int number_a, number_b, sw, total, number;
 
 void setup()
 {
@@ -15,40 +18,73 @@ void setup()
 
 void loop()
 {
+  int total, num1, num2;
+ 
  if(IR.IsDta()) // 데이터를 수신하였다면
  {
+ 
   IR.Recv(dta);
-  Serial.print(dta[D_DATA+3], HEX);
-  Serial.print("\n");
+  //Serial.print("입력 신호 :");
+  //Serial.print(dta[D_DATA+3], HEX);
+  //Serial.print("\n");
   
   data = String(dta[D_DATA+3], HEX);
   
-  data.toUpperCase();
-  Serial.print(data);
+  data.toUpperCase(); // 대문자 처리
 
-  numberset(data);  // 버튼 지정
-  
-  switch(case_num){
-    case 14 :
-          Serial.print("전원버튼입니다.");
-          break;
-    case 11 :
-          Serial.print("더하기를 진행합니다.");
-          break;
-    case 12 :
-          Serial.print("빼기를 진행합니다.");
-          break;
-    default :
-          Serial.print("지정 안된 버튼입니다.");
-  }
-  
+  num = numberset(data);  // 버튼 지정
+  //Serial.print("변환된 신호 :");
+  //Serial.print(num);
+  //Serial.print("\n");
+
+  if(data == "4F"){   // 리모컨의 C를 누를 경우
+  Serial.print("초기화");
+  total = number_a = number_b = sw = 0;
+  Serial.print(total);
+  Serial.print(number_a);
+  Serial.print(number_b);
   Serial.print("\n");
+  }else 
+    caculator(num); // 숫자 저장
  }
 }
 
-void numberset(String data){
-  if(data == "5D"){
+void caculator(int num){
+   if (num == 11){  // + 누르면
+    sw = 1;
+  }else if(num == 12){  // - 누르면
+    sw = 2;
+  } else if (num >= 0 && num <= 9){
+    if(sw == 0){
+      number_a = number_a *10 + num;
+      Serial.print("a에 저장된 숫자 :");
+      Serial.print(number_a);
+      Serial.print("\n");
+    }
+    else if(sw == 1 || sw == 2){
+     number_b = number_b *10 + num;
+      Serial.print("b에 저장된 숫자 :");
+      Serial.print(number_b);
+      Serial.print("\n"); 
+    }
+  }else if(num == 13 && sw == 1){
+    total = number_a + number_b;
+      Serial.print("합산 :");
+      Serial.print(total);
+      Serial.print("\n");
+  }else if(num == 13 && sw == 2){
+     total = number_a - number_b;
+      Serial.print("빼기 :");
+      Serial.print(total);
+      Serial.print("\n");
+  }
+   
+}
+
+int numberset(String data){
+  if(data == "5D"){ // 전원
     case_num = 14;
+    init_num = 0;
   }else if(data == "97"){
     case_num = 0;
   }else if(data == "CF"){
@@ -79,4 +115,5 @@ void numberset(String data){
     case_num = 13;
   }else case_num = 99;
 
+  return case_num;
 }
